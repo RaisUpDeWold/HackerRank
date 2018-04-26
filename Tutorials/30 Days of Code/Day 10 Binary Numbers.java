f0b04d4ecab24dc1a81a50f5fd525f290ace8354
Raisup Dewold <raisupdewold@gmail.com>
2018-04-27 02:39:08 +0400

    Solution for Day 10: Binary Numbers in Tutorials -> 30 Days of Code

---
+++ Tutorials/30 Days of Code/Day 10 Binary Numbers.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		String binary = Integer.toBinaryString(n);
+		int maxConsecutiveOneNum = 0;
+		int consecutiveOneNum = 0;
+		for (int i = 0; i < binary.length(); i++) {
+			if (binary.charAt(i) == '1') {
+				consecutiveOneNum++;
+				maxConsecutiveOneNum = Math.max(maxConsecutiveOneNum, consecutiveOneNum);
+			} else {
+				consecutiveOneNum = 0;
+			}
+		}
+		System.out.println(maxConsecutiveOneNum);
+
+		sc.close();
+    }
+}
