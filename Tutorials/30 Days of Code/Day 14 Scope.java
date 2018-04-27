55b26e7182d0ad95526d40526e2a88ffcbe5f43e
Raisup Dewold <raisupdewold@gmail.com>
2018-04-28 03:46:07 +0400

    Solution for Day 14: Scope in Tutorials -> 30 Days of Code

---
+++ Tutorials/30 Days of Code/Day 14 Scope.java
@@ -0,0 +1,12 @@
+	// Add your code here
+	Difference(int[] elements) {
+		this.elements = elements;
+	}
+
+	void computeDifference() {
+		for (int i = 0; i < elements.length; i++) {
+			for (int j = i + 1; j < elements.length; j++) {
+				maximumDifference = Math.max(maximumDifference, Math.abs(elements[i] - elements[j]));
+			}
+		}
+	}
