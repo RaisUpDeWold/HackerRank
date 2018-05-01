fc6ee76080debe952931af457a4064b2e1ac55b2
Raisup Dewold <raisupdewold@gmail.com>
2018-05-02 02:44:48 +0400

    Solution for Day 28: RegEx, Patterns, and Intro to Databases in Tutorials -> 30 Days of Code

---
+++ Tutorials/30 Days of Code/Day 28 RegEx, Patterns, and Intro to Databases.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        ArrayList<String> names = new ArrayList<>();
+        
+        for(int a0 = 0; a0 < N; a0++){
+            String firstName = in.next();
+            String emailID = in.next();
+            
+            if(emailID.contains("@gmail.com")){
+				names.add(firstName);
+			}
+        }
+        
+        Collections.sort(names);
+		for(String name : names){
+			System.out.println(name);
+		}
+    }
+}
