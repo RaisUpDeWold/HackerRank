c8de3217c530bf6edf75c1d65e38820e26c7c69b
Raisup Dewold <raisupdewold@gmail.com>
2018-07-02 09:18:03 +0400

    Solution for "Exceptional Server" in C++/Classes

---
+++ C++/Classes/Exceptional Server.cpp
@@ -0,0 +1,11 @@
+		/* Enter your code here. */
+        try{
+            int x=Server::compute(A,B);
+            cout<<x<<endl;
+        }catch(bad_alloc& e){
+            cout<<"Not enough memory"<<'\n';
+        }catch(exception &e){
+            cout<<"Exception: "<<e.what()<<'\n';
+        }catch(...){
+            cout<<"Other Exception"<<'\n';
+        }
