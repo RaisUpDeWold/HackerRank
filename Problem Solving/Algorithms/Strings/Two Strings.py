3d3ffd4fa301c696ae8dfa414252724419f19783
Raisup Dewold <raisupdewold@gmail.com>
2018-07-15 15:37:34 +0400

    Solution for "Two Strings" in Problem Solving -> Algorithms -> Strings

---
+++ Problem Solving/Algorithms/Strings/Two Strings.py
@@ -0,0 +1,12 @@
+t = int(raw_input())
+
+for _ in xrange(t):
+
+    a = set(raw_input())
+    b = set(raw_input())
+
+    if len(a.intersection(b)) > 0:
+        print "YES"
+    else:
+        print "NO"
+        
