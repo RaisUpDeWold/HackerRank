1b4d42fbe87b9888587fcd2a7a50e2ab44e54990
Raisup Dewold <raisupdewold@gmail.com>
2018-07-19 01:25:47 +0400

    Solution for "Zipped!" in Python -> Built-Ins

---
+++ Python/Built-Ins/Zipped!.py
@@ -0,0 +1,16 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N, X = map(int, raw_input().split())
+
+subjects = list()
+
+for i in range(0, X):
+    subjects.append(list(map(float, raw_input().split())))
+
+students = zip(*subjects)
+
+for i in range(0, N):
+    marks = 0.0
+    for j in range(0, X):
+        marks += students[i][j]
+    print marks / X
+    
