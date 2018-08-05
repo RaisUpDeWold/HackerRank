6fe1e1237b92f9160216861c4b7264fe01dd6967
Raisup Dewold <raisupdewold@gmail.com>
2018-08-05 07:02:34 +0400

    Solution for "Mean, Var, and Std" in Python -> Numpy

---
+++ Python/Numpy/Mean, Var, and Std.py
@@ -0,0 +1,5 @@
+import numpy as np
+np.set_printoptions(sign=' ')
+n,m= map(int,input().split())
+a=np.array([input().split() for _ in range(n)],int)
+print(np.mean(a,axis=1),np.var(a,axis=0),np.round(np.std(a),12),sep='\n')
