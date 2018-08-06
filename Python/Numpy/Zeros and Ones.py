a398ad30771cd917a286a9120fbc2c1edad3b213
Raisup Dewold <raisupdewold@gmail.com>
2018-08-07 02:53:47 +0400

    Solution for "Zeros and Ones" in Python -> Numpy

---
+++ Python/Numpy/Zeros and Ones.py
@@ -0,0 +1,7 @@
+import numpy
+
+if __name__ == "__main__":
+    data = tuple(map(int, input().split()))
+    print(numpy.zeros(data, dtype = numpy.int))
+    print(numpy.ones(data, dtype = numpy.int))
+    
