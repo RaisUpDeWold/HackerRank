60c66927c15eae5ab81b250d1a3e5e0b36fc3661
Raisup Dewold <raisupdewold@gmail.com>
2018-08-21 00:29:43 +0400

    Solution for "String Validators" in Python -> Strings

---
+++ Python/Strings/String Validators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    S = raw_input()
+
+    print any(c.isalnum() for c in S)
+    print any(c.isalpha() for c in S)
+    print any(c.isdigit() for c in S)
+    print any(c.islower() for c in S)
+    print any(c.isupper() for c in S)
