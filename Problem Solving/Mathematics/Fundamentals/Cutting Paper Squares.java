9a798c90c7d26c952c60e9420fdd63718e955db9
Raisup Dewold <raisupdewold@gmail.com>
2018-10-02 00:33:52 +0400

    Solution for "Cutting Paper Squares" in Problem Solving -> Mathematics -> Fundamentals

---
+++ Problem Solving/Mathematics/Fundamentals/Cutting Paper Squares.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long n = scan.nextLong();
+        long m = scan.nextLong();
+        scan.close();
+        
+        System.out.println(n * m - 1);
+    }
+}
