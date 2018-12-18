e27bc0dd55631f0929e8bf670762137991659b0b
Raisup Dewold <raisupdewold@gmail.com>
2018-12-19 01:16:53 +0400

    Solution for "Twins" in Problem Solving -> Mathematics -> Number Theory

---
+++ Problem Solving/Mathematics/Number Theory/Twins.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+const int MAXN = 1000000 + 100;
+
+int l, r;
+bool isPrime[MAXN];
+
+int main(int argc, const char * argv[]) {
+    cin >> l >> r;
+    assert(1 <= l && l <= r && r <= 1000000000);
+    assert(r - l <= 1000000);
+
+    for(int i = 0; i <= r - l; i++)
+        isPrime[i] = true;
+    for(int i = 2; i * i <= r; i++) {
+        int startPos = l / i * i;
+        if (startPos < l)
+            startPos += i;
+        if (startPos == i)
+            startPos += i;
+        for(int j = startPos; j <= r; j += i)
+            isPrime[j - l] = false;
+    }
+
+    int ret = 0;
+
+    for(int i = 0 + (l == 1); i <= r - l - 2; i++)
+        ret += isPrime[i] & isPrime[i + 2];
+
+    cout << ret << endl;
+
+    return 0;
+}
